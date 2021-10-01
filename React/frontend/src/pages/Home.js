5d5803a7a82e139d73d41c0d6e72e47aad5f5fab
Ekke Alar Toomingas <ekkealar@tlu.ee>
2021-10-01 21:37:13 +0300

    2. Homework

--- React/frontend/src/pages/Home.js
+++ React/frontend/src/pages/Home.js
@@ -1,11 +1,52 @@
-import Item from "../components/Item";
+import ItemList from "../components/ItemList";
+import CategoryList from "../components/CategoryList";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function Home() {
+	const [isLoading, setIsLoading] = useState(true);
+	const [loadedItems, setLoadedItems] = useState([]);
+	const [loadedCategories, setLoadedCategories] = useState([]);
+
+	useEffect(() => {
+		fetch("http://localhost:8080/items")
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				console.log(data);
+				setIsLoading(false);
+				setLoadedItems(data);
+			});
+	}, []);
+
+	useEffect(() => {
+		fetch("http://localhost:8080/categories")
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				console.log(data);
+				setIsLoading(false);
+				setLoadedCategories(data);
+			});
+	}, []);
+
+	if (isLoading) {
+		return <div>Laeb...</div>;
+	}
+
 	return (
 		<div>
-			Koduleht
-			<Item name="Item1" price="10" category="phones" />
-			<Item name="Item2" price="30" category="tablet" />
+			<Link to="add-item">
+				<button>Lisa uus</button>
+			</Link>
+			<ItemList items={loadedItems} />
+			<br />
+			<Link to="add-category">
+				<button>Lisa kategooria</button>
+			</Link>
+			<CategoryList categories={loadedCategories} />
 		</div>
 	);
 }
